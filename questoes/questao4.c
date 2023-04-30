a6bc40efe457262d4b69d8f44c29f4c6d6f16da8
Deborah Hoepers <deborah.ghps@gmail.com>
2023-04-29 21:50:58 -0300

    Enunciado e resolução da questão 4

---
+++ questoes/questao4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    float real, dolar;
+
+    printf("Converta Real para Dólar");
+    printf("Insira a quantidade desejada em reais:\n");
+    scanf("%f", &real);
+
+    dolar = real/3.50;
+        printf("A quantidade convertida para dólar é de: U$ %f", dolar);
+
+    return 0;
+
+}
